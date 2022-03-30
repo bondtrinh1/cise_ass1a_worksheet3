378ed28305935ba0144c1f64dbb4f92b3e39ab35
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2022-03-30 15:41:33 +1300

    yo

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('Hello world!'));
 app.use('/api/books', books);
+app.use(express.static("build"));
 
 
 const path = require("path");
@@ -17,7 +18,7 @@ const path = require("path");
 app.use(express.static(path.resolve(__dirname, "./cise_ass1a_worksheet3_frontend/build")));
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./cise_ass1a_worksheet3_frontend/build", "index.html"));
-}); 
+});
 
 const port = process.env.PORT || 8082;
 
