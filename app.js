74a4e968e19d3afff140277018719aee40a5df8f
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2022-03-30 15:34:24 +1300

    yo

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('Hello world!'));
 app.use('/api/books', books);
-app.use(express.static("build"));
+
 
 const path = require("path");
 
